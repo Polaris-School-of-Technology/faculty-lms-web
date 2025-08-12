405e89d9df67d9adaf6d516bafbca055266da00c
Digambar <ddjail2004@gmail.com>
2025-08-12 11:32:26 +0530

    updated: max requests to 10

--- src/constants/app.js
+++ src/constants/app.js
@@ -5,4 +5,4 @@ export const QR_REFRESH_TIME = 5;
 export const QR_REFRESH_INTERVAL = QR_REFRESH_TIME * 1000;
 
 // Maximum number of QR code generation requests
-export const MAX_QR_REQUESTS = 5;
+export const MAX_QR_REQUESTS = 10;
