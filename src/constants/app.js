c2f0c4096cdd03dbba2a6db6b425b5c136fcbbf3
Digambar <ddjail2004@gmail.com>
2025-08-25 19:54:32 +0530

    updated timer to 5 seconds

--- src/constants/app.js
+++ src/constants/app.js
@@ -1,5 +1,5 @@
 // QR Code refresh time in seconds
-export const QR_REFRESH_TIME = 10;
+export const QR_REFRESH_TIME = 5;
 
 // Convert to milliseconds for JavaScript timers
 export const QR_REFRESH_INTERVAL = QR_REFRESH_TIME * 1000;
